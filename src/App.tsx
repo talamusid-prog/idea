f98c2f45255650765a195e17260eed6dab83d6b6
talamusid-prog <email-talamusid-prog@gmail.com>
2025-08-15 14:19:07 +0800

    feat: tambahkan Vercel Analytics untuk tracking pengunjung dan page views

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
+import { Analytics } from "@vercel/analytics/react";
 import ErrorBoundary from "./components/ErrorBoundary";
 import Index from "./pages/Index";
 import Admin from "./pages/Admin";
@@ -40,6 +41,8 @@ const App = () => (
               <Route path="*" element={<NotFound />} />
             </Routes>
           </BrowserRouter>
+          {/* Analytics component untuk melacak pengunjung dan page views */}
+          <Analytics />
         </TooltipProvider>
       </QueryClientProvider>
     </HelmetProvider>
